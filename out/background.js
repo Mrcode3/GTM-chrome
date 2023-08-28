5147cbc6f130b5bd8f3bea684c737e2e21b21134
Zechao Lin <zechao.lin001@umb.edu>
2023-08-28 16:22:44 -0400

    edit background.js

--- out/background.js
+++ out/background.js
@@ -1,6 +1,9 @@
 console.log("hi");
 
-// THIS DOES NOT WORK - source: https://stackoverflow.com/questions/74414895/how-to-open-chrome-extension-on-new-window-instead-of-the-default-popup
+// THIS DOES NOT WORK
+// source: https://stackoverflow.com/questions/74414895/how-to-open-chrome-extension-on-new-window-instead-of-the-default-popup
+//         https://stackoverflow.com/questions/47314799/how-do-i-open-a-chrome-extension-in-its-own-window
+//
 // chrome.browserAction.onClicked.addListener(function (activeTab) {
 //   chrome.windows.create({
 //     url: chrome.runtime.getURL("index.html"),
