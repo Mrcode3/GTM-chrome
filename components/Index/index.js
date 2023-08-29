b59e3995f52ae720ba8789b794f765cefb13234b
Zechao Lin <zechao.lin001@umb.edu>
2023-08-28 20:57:23 -0400

    add options

--- components/Index/index.js
+++ components/Index/index.js
@@ -4,15 +4,34 @@ export default function Index({ navigateToPage }) {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <div>options</div>
-        <h2 className={styles.title}>
-          GTM Copilot <br /> is now ready
-        </h2>
-        <p className={styles.description}>You may visit a supported site</p>
-        <button className={styles.button}>See example</button>
-        {/* <h1 className={styles.code}>Index Page ./components/Index/index.js</h1>
-        <p>{"[ - This is Index page content - ]"}</p>
-        <p onClick={() => navigateToPage("new")}>{"Go to New Page >"}</p> */}
+        {/* options components -- start*/}
+        <div className={styles.options}>
+          <div className={styles.lists}>
+            <p>List</p>
+            <select name="" id="">
+              <option value="">My Leads</option>
+              <option value="a">a</option>
+              <option value="b">b</option>
+              <option value="c">c</option>
+              <option value="d">d</option>
+            </select>
+          </div>
+          <button className={styles.toggle}>
+            <p>hide options</p>
+            <img src="/up-arrow.png" alt="" />
+          </button>
+        </div>
+        {/* options components -- end*/}
+
+        {/* content component --start */}
+        <div className={styles.content}>
+          <h2 className={styles.title}>
+            GTM Copilot <br /> is now ready
+          </h2>
+          <p className={styles.description}>You may visit a supported site</p>
+          <button className={styles.button}>See example</button>
+        </div>
+        {/* content component -- end */}
       </main>
     </div>
   );
